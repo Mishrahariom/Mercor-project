3b85390a6957c3ac03a46868421d1aff65e29094
Mishrahariom <hariom.cs2020@mpgi.edu.in>
2023-09-10 15:48:20 +0530

    chanegs on navbar section

--- src/Section/Hero.jsx
+++ src/Section/Hero.jsx
@@ -1,37 +1,100 @@
-function Hero() {
-  return ( 
-    <section >
-      <div style={{ "padding": "35px" }} class="container">
-      
-        <div class="row">
-          <div style={{ "margin-right": "1.25rem" }} class="col">
-            <img src='https://i.ibb.co/Mkwqs0Z/logo.png'></img>
-          </div>
-          <div class="col-10">
-            <nav className="navbar">
-              <ul className="navbar-links">
-                <li><a href="/">SIGN IN </a></li>
-                <li><a href="/about">LEGAL</a></li>
-                <li><a href="/services">LICENSES</a></li>
-                <li><a href="/contact">SECURITY</a></li>
-                <li><a href="/contact">CAREERS</a></li>
-                <li><a href="/contact">PRESS</a></li>
-                <li><a href="/contact">SUPPORT</a></li>
-                <li><a href="/contact">STATUS</a></li>
-                <li><a href="/contact">CODEBLOG</a></li>
-              </ul>
-            </nav>
-          </div>
-          <div style={{ "margin-left": "1.25rem" }} class="col">
-            <img src='https://i.ibb.co/brbtVxt/Layer-2.png'></img>
-          </div>
-        </div>
+import React, { useState } from 'react';
+
+
+const Hero = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navbarLinksClass = isMenuOpen ? 'navbar-links open' : 'navbar-links';
+
+  return (
+    <nav className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+      <div className="navbar-logo">
+        {/* <h1>Your Website</h1> */}
+        <img src="logo.png" alt="Logo" />
       </div>
-    </section>
+      <div className={`navbar-toggle`} onClick={toggleMenu}>
+        <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+      </div>
+
+      <div className={`navbar-photo ${isMenuOpen ? 'open' : ''}`}>
+        <img src="https://i.ibb.co/M8jMbFz/Layer-2-1.png" alt="User" />
+      </div>
+
+      <div className={navbarLinksClass}>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+        <a href="/contact">Contact</a>
+        <a href="/contact">Contact</a>
+        <a href="/contact">Contact</a>
+        <a href="/contact">Contact</a>
+        <a href="/contact">Contact</a>
+      </div>
+
+      <div className={`navbar-photo d-none ${isMenuOpen ? 'open' : ''}`}>
+        <img src="https://i.ibb.co/M8jMbFz/Layer-2-1.png" alt="User" />
+      </div>
+   
+    </nav>
   );
-}
+};
 
 export default Hero;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { FaBars, FaTimes } from 'react-icons/fa';
+
+// const Hero = () => {
+//   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+//   const toggleMobileMenu = () => {
+//     setIsMobileMenuOpen(!isMobileMenuOpen);
+//   };
+
+//   return (
+//     <nav className="navbar">
+//       <div className="navbar-brand">
+//         Logo
+//       </div>
+//       <div className={`navbar-links ${isMobileMenuOpen ? 'mobile-open' : ''}`}>
+//         <a href="/">Home</a>
+//         <a href="/about">About</a>
+//         <a href="/services">Services</a>
+//         <a href="/contact">Contact</a>
+//       </div>
+//       <div className="navbar-toggle" onClick={toggleMobileMenu}>
+//         {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Hero;
+
+
+
 
 
 
